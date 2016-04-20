42552915664724d493e346aea48a328d5103582a
Noel Worden <noelworden@gmail.com>
2016-04-19 20:53:05 -0600

    updated seeds and random_data library

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 require 'random_data'
+
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+    )
+end
+
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
@@ -20,7 +29,12 @@ posts = Post.all
     )
 end
 
+user = User.first
+  user.email = 'noelworden@gmail.com',
+  user.password = 'helloworld'
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  let(:my_user) {User.create!(name: "Bloccit User", email: "user@bloccit.io", password: "helloworld")}
-  let(:my_topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
-  let(:my_post) {my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user)}
+  let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+  let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
 
-  describe "GET show" do
+describe "GET show" do
 
     it "returns http success" do
       get :show, topic_id: my_topic.id, id: my_post.id
@@ -45,7 +46,7 @@ RSpec.describe PostsController, type: :controller do
   describe "POST create"do
 
     it "increases the number of a Post by 1" do
-      expect{post :create, topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+      expect{post :create, topic_id: my_topic.id, id: my_post.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
     end
 
     it "assigns the new post to @post" do
