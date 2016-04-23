936b73e10ff5de467f12baddaa68b89f4ae9feb7
Noel Worden <noelworden@gmail.com>
2016-04-23 11:46:59 -0600

    all tasks in checkpoint, authorizing users to perform certain tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,9 +29,18 @@ posts = Post.all
     )
 end
 
-user = User.first
-  user.email = 'noelworden@gmail.com',
-  user.password = 'helloworld'
+admin = User.create!(
+  name:    'Admin User',
+  email:   'admin@example.com',
+  password:'helloworld',
+  role:    'admin'
+  )
+
+member = User.create!(
+  name:   'Member User',
+  email:  'member@example.com',
+  password:'helloworld'
+  )
 
 puts "Seed finished"
 puts "#{User.count} users created"
