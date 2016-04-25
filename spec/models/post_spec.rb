63ece319bb9f053b757b25f36c027f1f3b084a6c
Noel Worden <noelworden@gmail.com>
2016-04-25 17:43:28 -0600

    fixed seed issue

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe Post, type: :model do
   it {is_expected.to belong_to(:user)}
   it {is_expected.to validate_presence_of(:title)}
   it {is_expected.to validate_presence_of(:body)}
-  #it {is_expected.to validate_presence_of(:topic)}
-  #it {is_expected.to validate_presence_of(:user)}
+  it {is_expected.to validate_presence_of(:topic)}
+  it {is_expected.to validate_presence_of(:user)}
 
   it {is_expected.to validate_length_of(:title).is_at_least(5)}
   it {is_expected.to validate_length_of(:body).is_at_least(20)}
@@ -27,6 +27,7 @@ RSpec.describe Post, type: :model do
     it "has a title, body, and user attribute" do
       p user
       p topic
+      p post
       expect(post).to have_attributes(title: title, body: body, user: user)
     end
   end
