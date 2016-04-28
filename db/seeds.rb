cdd6f8805d08c06da471aa1a463d4f7f5b3d81cf
Noel Worden <noelworden@gmail.com>
2016-04-28 16:42:56 -0600

    seeded comment data. made field to enter comment. made delete link for auth users

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,18 +35,18 @@ posts = Post.all
     )
 end
 
-# admin = User.create!(
-#   name:    'Admin User',
-#   email:   'admin@example.com',
-#   password:'helloworld',
-#   role:    'admin'
-#   )
+admin = User.create!(
+  name:    'Admin User',
+  email:   'admin@example.com',
+  password:'helloworld',
+  role:    'admin'
+  )
 
-# member = User.create!(
-#   name:   'Member User',
-#   email:  'member@example.com',
-#   password:'helloworld'
-#   )
+member = User.create!(
+  name:   'Member User',
+  email:  'member@example.com',
+  password:'helloworld'
+  )
 
 puts "Seed finished"
 puts "#{User.count} users created"
