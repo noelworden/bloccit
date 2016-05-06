9fed54a69223b5b2f44acc396dcbcf02ece9ff36
Noel Worden <noelworden@gmail.com>
2016-05-05 18:54:28 -0600

    got the voting links on the page

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -29,10 +29,34 @@ RSpec.describe Post, type: :model do
   describe "attributes" do
 
     it "has a title, body, and user attribute" do
-      p user
-      p topic
-      p post
       expect(post).to have_attributes(title: title, body: body, user: user)
     end
   end
+
+  describe "voting" do
+    before do
+      3.times { post.votes.create!(value: 1)}
+      2.times { post.votes.create!(value: -1)}
+      @up_votes = post.votes.where(value: 1).count
+      @down_votes = post.votes.where(value: -1).count
+    end
+
+    describe "up_votes" do
+      it "counts the number of votes with the value = 1" do
+        expect(post.up_votes).to eq(@up_votes)
+      end
+    end
+
+    describe "down_votes" do
+      it "counts the number of votes with value = -1" do
+        expect(post.down_votes).to eq(@down_votes)
+      end
+    end
+
+    describe "points" do
+      it "returns the sum of all the down and up votes" do
+        expect(post.points).to eq(@up_votes - @down_votes)
+      end
+    end
+  end 
 end
