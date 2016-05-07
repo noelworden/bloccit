3236a5ef17d1d551b04c4c15c85f3fa145361a8c
Noel Worden <noelworden@gmail.com>
2016-05-07 13:48:27 -0600

    able to unfavorite a post

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,30 @@
 class FavoritesController < ApplicationController
+
+  before_action :require_sign_in
+
+  def create
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: post)
+
+    if favorite.save
+      flash[:notice] = "Post favorited, hombre"
+    else
+      flash[:alert] = "Favoriting failed, amigo"
+    end
+
+    redirect_to [post.topic, post]
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+
+    if favorite.destroy
+      flash[:notice] = "Unfavorited, hombre"
+    else
+      flash[:alert] = "Unfavoriting failed, amigo"
+    end
+
+    redirect_to [post.topic, post]
+  end
 end
