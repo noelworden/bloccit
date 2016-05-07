aa3cf585e9e9eb631321dc238f639cb40f53f6e1
Noel Worden <noelworden@gmail.com>
2016-05-07 13:12:15 -0600

    configured favorites and got the button to show up on posts-show

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,10 +12,9 @@ RSpec.describe Post, type: :model do
 
   it {is_expected.to have_many(:labelings)}
   it {is_expected.to have_many(:labels).through(:labelings)}
-
   it {is_expected.to have_many(:comments)}
   it {is_expected.to have_many(:votes)}
-
+  it {is_expected.to have_many(:favorites)}
   it {is_expected.to belong_to(:topic)}
   it {is_expected.to belong_to(:user)}
   it {is_expected.to validate_presence_of(:title)}
