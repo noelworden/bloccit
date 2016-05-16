6202e1e2a52bce066074492d1226f12bd52c8cf3
Noel Worden <noelworden@gmail.com>
2016-05-15 19:46:38 -0600

    completed up through the POST section

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,4 +12,23 @@ class Api::V1::UsersController <Api::V1::BaseController
     users = User.all
     render json: users, status: 200
   end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update_attributes(user_params)
+      render json: user, status: 200
+    else
+      render json: {error: "User update failed", status: 400}, status: 400
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
 end
