ec6c4b3b183120598708599e53075c63d4f4ae28
Noel Worden <noelworden@gmail.com>
2016-05-17 19:26:00 -0600

    added tests and controllers for user-post

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -24,6 +24,14 @@ class Api::V1::UsersController <Api::V1::BaseController
   end
 
   def create
+    user = User.new(user_params)
+
+    if user.valid?
+      user.save!
+      render json: user, status: 201
+    else
+      render json: { error: "User is invalid", status: 400}, status: 400
+    end
   end
 
   private
